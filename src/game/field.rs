afb0746f7f5469fdf34951d73baeb43a4325c66f
Mark Tyrkba <awfulmarco@gmail.com>
2023-12-15 20:49:47 +0300

    Fixed bug allowing continuous play as a single side. Added GameError enum for error handling

--- src/game/field.rs
+++ src/game/field.rs
@@ -1,65 +1,79 @@
-pub(crate) mod field
-{
-use crossterm::{cursor, execute, terminal};
+pub(crate) mod field {
+    use crossterm::{cursor, execute, terminal};
+    use crate::GameError;
 
-#[derive(Clone)]
-pub struct Field {
-    pub field: Vec<Vec<char>>,
-}
-
-impl Field {
-    pub fn new() -> Self {
-        Field { field: vec![vec!['_'; 3]; 3] }
+    pub struct Field {
+        pub field: Vec<Vec<char>>
     }
 
-    pub fn make_move(&mut self, row: usize, col: usize, player: char) -> Result<(), &'static str>{
-        if row < self.field.len() && col < self.field[0].len() {
-            if self.field[row][col] == '_' {
-                self.field[row][col] = player;
-                Ok(())
-            } else { Err("Invalid move: cell already occupied") }
-        } else { Err("Invalid move: out of bounds") }
-    }
+    impl Field {
+        pub fn new() -> Self {
+            Field {
+                field: vec![vec!['_'; 3]; 3],
+            }
+        }
 
-    pub fn clear_console(&self) {
-        execute!(std::io::stdout(), terminal::Clear(terminal::ClearType::All), cursor::MoveTo(0, 0)).unwrap();
-    }
+        pub fn make_move(
+            &mut self,
+            row: usize,
+            col: usize,
+            player: char,
+        ) -> Result<(), GameError> {
+            if row < self.field.len() && col < self.field[0].len() {
+                if self.field[row][col] == '_' {
+                    self.field[row][col] = player;
+                    Ok(())
+                }
+                else
+                    { Err(GameError::InvalidMove("Cell already occupied".to_string())) } }
+            else
+                { Err(GameError::OutOfBounds) }
+        }
 
-    pub fn is_over(&self) -> Option<char> {
-        for row in &self.field {
-            if row.iter().all(|&cell| cell == 'X')
+        pub fn clear_console(&self) {
+            execute!(
+                std::io::stdout(),
+                terminal::Clear(terminal::ClearType::All),
+                cursor::MoveTo(0, 0)
+            )
+                .unwrap();
+        }
+
+        pub fn is_over(&self) -> Option<char> {
+            for row in &self.field {
+                if row.iter().all(|&cell| cell == 'X')
+                    { return Some('X'); }
+                else if row.iter().all(|&cell| cell == 'O')
+                    { return Some('O'); }
+            }
+
+            for col in 0..self.field[0].len() {
+                if self.field.iter().all(|row| row[col] == 'X')
+                    { return Some('X'); }
+                else if self.field.iter().all(|row| row[col] == 'O')
+                    { return Some('O'); }
+            }
+
+            if (0..self.field.len()).all(|i| i < self.field[i].len() && self.field[i][i] == 'X')
                 { return Some('X'); }
-            else if row.iter().all(|&cell| cell == 'O')
+            else if (0..self.field.len()).all(|i| i < self.field[i].len() && self.field[i][i] == 'O')
                 { return Some('O'); }
-        }
 
-        for col in 0..self.field[0].len() {
-            if self.field.iter().all(|row| row[col] == 'X')
+            if (0..self.field.len()).all(|i| i < self.field[i].len() && self.field[i][self.field.len() - 1 - i] == 'X')
                 { return Some('X'); }
-            else if self.field.iter().all(|row| row[col] == 'O')
+            else if (0..self.field.len()).all(|i| i < self.field[i].len() && self.field[i][self.field.len() - 1 - i] == 'O')
                 { return Some('O'); }
+
+            if self.field.iter().any(|row| row.iter().any(|&cell| cell == '_'))
+                { return None; }
+            Some('D')
         }
 
-        if (0..self.field.len()).all(|i| self.field[i][i] == 'X')
-            { return Some('X'); }
-        else if (0..self.field.len()).all(|i| self.field[i][i] == 'O')
-            { return Some('O'); }
-
-        if (0..self.field.len()).all(|i| self.field[i][self.field.len() - 1 - i] == 'X')
-            { return Some('X'); }
-        else if (0..self.field.len()).all(|i| self.field[i][self.field.len() - 1 - i] == 'O')
-            { return Some('O'); }
-
-        if self.field.iter().any(|row| row.iter().any(|&cell| cell == '_')) { return None; }
-        Some('D')
-    }
-
-
-    pub fn field_out(&self) {
-        for row in &self.field {
-            let row_str: String = row.iter().collect();
-            println!("{row_str}");
+        pub fn field_out(&self) {
+            for row in &self.field {
+                let row_str: String = row.iter().collect();
+                println!("{row_str}");
+            }
         }
     }
-}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod game;
 use game::field;
 
-use std::{io, io::Write};
-use std::time::Duration;
+use std::{io, time::Duration};
 use crossterm::{
     cursor,
     execute,
     event::{self, KeyCode, KeyEvent},
+    terminal,
 };
+
+enum GameError {
+    InvalidMove(String),
+    OutOfBounds,
+}
 
 struct Game {
     field: field::field::Field,
@@ -37,66 +42,65 @@ impl Game {
     fn make_move(&mut self) {
         match self.field.make_move(self.cursor_row, self.cursor_col, self.player) {
             Ok(()) => {
+                self.switch_player();
                 self.field.clear_console();
                 self.field.field_out();
                 match self.field.is_over() {
-                    Some(winner) => {
-                        match winner {
-                            'D' => println!("DRAW"),
-                             _ => println!("Winner's a:{winner}")
-                        }
-                        std::thread::sleep(Duration::from_millis(3000));
-                        std::process::exit(0);
-                    },
+                    Some('D') => println!("DRAW"),
+                    Some(winner) => println!("Winner's a: {}", winner),
                     None => {}
                 }
             }
-            Err(err) => match self.cursor_row {
-                0 => print!("\n\n\nError: {err}"),
-                1 => print!("\n\nError: {err}"),
-                2 => print!("\nError: {err}"),
-                _ => print!("Error: {err}"),
-            },
+            Err(err) => self.print_error(err),
         }
     }
 
     fn handle_key_event(&mut self, code: KeyCode) {
         match code {
-            KeyCode::Esc => { std::process::exit(0); }
-            KeyCode::Enter => {
-                self.make_move();
-                self.switch_player();
-            }
-            KeyCode::Up   => { if self.cursor_row > 0 { self.cursor_row -= 1; } }
-            KeyCode::Down => { if self.cursor_row < 2 { self.cursor_row += 1; } }
-            KeyCode::Left => { if self.cursor_col > 0 { self.cursor_col -= 1; } }
-            KeyCode::Right=> { if self.cursor_col < 2 { self.cursor_col += 1; } }
+            KeyCode::Esc => std::process::exit(0),
+            KeyCode::Enter => { self.make_move(); }
+            KeyCode::Up if self.cursor_row > 0 => self.cursor_row -= 1,
+            KeyCode::Down if self.cursor_row < 2 => self.cursor_row += 1,
+            KeyCode::Left if self.cursor_col > 0 => self.cursor_col -= 1,
+            KeyCode::Right if self.cursor_col < 2 => self.cursor_col += 1,
             _ => {}
         }
     }
 
     fn run(&mut self) {
         loop {
-            execute!(io::stdout(),
-                cursor::MoveTo(self.cursor_col as u16, self.cursor_row as u16))
-                    .unwrap();
+            execute!(io::stdout(),cursor::MoveTo(self.cursor_col as u16, self.cursor_row as u16))
+                .unwrap();
 
             if event::poll(Duration::from_millis(100)).unwrap() {
-                if let event::Event::Key(KeyEvent { code, .. }) = event::read().unwrap()
-                {
+                if let event::Event::Key(KeyEvent { code, .. }) = event::read().unwrap() {
                     self.handle_key_event(code);
                     event::read().unwrap();
                 }
             }
         }
     }
+
+    fn print_error(&self, err: GameError) {
+        match err {
+            GameError::InvalidMove(message) => {
+                match self.cursor_row {
+                    0 => println!("\n\n\nError: {}", message),
+                    1 => println!("\n\nError: {}", message),
+                    2 => println!("\nError: {}", message),
+                    _ => println!("Error: {}", message),
+                }
+            }
+            GameError::OutOfBounds => { println!("Error: Invalid move - out of bounds"); }
+        }
+    }
 }
 
 fn main() {
-    crossterm::terminal::enable_raw_mode().unwrap();
+    terminal::enable_raw_mode().unwrap();
     let mut game = Game::new();
     game.field.clear_console();
     game.field.field_out();
     game.run();
-    crossterm::terminal::disable_raw_mode().unwrap();
+    terminal::disable_raw_mode().unwrap();
 }
